292e2a22da3af3bdf37c7c4f4a79daaae6740872
emirhancakirct <cakiremirhanct@gmail.com>
2022-02-15 13:14:37 -0500

    Find-the-Number-Game

---
+++ app.js
@@ -0,0 +1,59 @@
+let guess = document.getElementById("number");
+let check = document.getElementById("check");
+let attempts10 = document.getElementById("attempts10");
+let playAgain = document.getElementById("playAgain");
+let attempts = document.getElementById("attempts-container");
+let writeNumber = document.getElementById("p2");
+let nOfAttempts = document.getElementById("attempts");
+let luckyNum = Math.floor(Math.random() * 100 + 1);
+let numbers = [0,100]
+let x;
+
+
+check.addEventListener("click", () => {
+
+if (guess.value == luckyNum){
+    p2.style.fontSize="2rem";
+    p2.innerText = `Congratulations, you found the random number ${luckyNum} on your ${11-Number(attempts10.innerText)}. attempts`
+    attempts.style.display = "none";
+    playAgain.style.display = "block";
+    guess.value=""
+}
+else if (guess.value>luckyNum){
+    p2.style.fontSize="2rem";
+    numbers.push(guess.value);
+    numbers.sort(function(a,b){return a-b});
+    x = numbers.indexOf(guess.value);
+    p2.innerText = `Please enter a smaller number, between ${numbers[x-1]} and ${numbers[x]}`
+    attempts10.innerText=Number(attempts10.innerText)-1
+    if(attempts10.innerText==0){
+        p2.innerText=`Sorry, you failed. The number was ${luckyNum}`;
+        attempts.style.display="none";
+        playAgain.style.display="block";
+        guess.value=""
+        
+    }
+}
+else if(guess.value<luckyNum){
+p2.style.fontSize="2rem";
+numbers.push(guess.value);
+numbers.sort(function(a,b){return a-b});
+x = numbers.indexOf(guess.value);
+p2.innerText=`Please enter a greater number, between ${numbers[x]} and ${numbers[x+1]}`
+attempts10.innerText=Number(attempts10.innerText)-1
+if(attempts10.innerText==0){
+    p2.innerText=`Sorry, you failed. The number was ${luckyNum}`
+    attempts.style.display="none";
+    playAgain.style.display="block";
+    guess.value=""
+}
+}
+})
+
+playAgain.addEventListener("click", ()=>{
+    attempts10.innerText="10";
+    attempts.style.display = "flex";
+    playAgain.style.display = "none";
+    p2.innerText="Write the number"
+    numbers=[0,100]
+    })
